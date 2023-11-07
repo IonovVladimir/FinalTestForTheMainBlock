9c58e042f6527dcf24a5f7c00e0cdeb48704605e
Vladimir <vladimir.ionov.blin@mail.ru>
2023-11-07 23:23:18 +0300

    задал входной массив

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Drawing;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Задача: Написать программу, которая из имеющегося массива строк
+        //формирует новый массив из строк, длина которых меньше,
+        //либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры,
+        //либо задать на старте выполнения алгоритма. При решении не рекомендуется
+        //пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+        //функция чтения с консоли и перевода в целые числа
+        int InputStrToInt(string text) 
+        {
+            Console.WriteLine(text);
+            return Convert.ToInt32(Console.ReadLine()); 
+        }
+
+        //функция заполнения массива с консоли (работает ли?)
+        void FillStrArray(string[] strAr, int n ) 
+        {
+            for (int i = 0; i < n; i++) 
+            {
+                Console.WriteLine($"Введи {i} элемент массива");
+                strAr[i] = Console.ReadLine();
+            }
+        }
+
+        //функция вывода элементов массива
+        void PrintStrArray(string[] strAr, int n) 
+        {
+            for (int i = 0; i < n; i++) 
+            { 
+                Console.WriteLine($"{i}: {strAr[i]} ");
+            }
+        }
+
+
+        int n = InputStrToInt("Введи размер входного массива = ");
+        Console.WriteLine($"Входной массив состоит из {n} элементов.");
+        string[] strAr = new string[n];
+
+        FillStrArray(strAr, n );
+        Console.WriteLine();
+        Console.WriteLine("~~~~~~~~~");
+        Console.WriteLine();
+        PrintStrArray(strAr, n);
+
+
+
+    }
+}
